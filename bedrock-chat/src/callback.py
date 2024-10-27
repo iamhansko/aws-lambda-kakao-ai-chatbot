cd37f2145c396710a3c012d8fd45ba0a90dd229e
iamhansko <iamhansko@kakao.com>
2024-10-27 11:05:56 +0900

    infra: SAM Template Integration

--- bedrock-chat/src/callback.py
+++ bedrock-chat/src/callback.py
@@ -1,23 +1,27 @@
 import requests
-from langchain_aws import ChatBedrock
+import json
+import boto3
 
 # https://python.langchain.com/docs/integrations/llms/bedrock/
-def bedrock_chatbot(input_text):
-    bedrock_llm = ChatBedrock(
-        credentials_profile_name='default',
-        model_id='anthropic.claude-3-5-sonnet-20240620-v1:0',
-        model_kwargs={
-            "temperature": 0.5,
-            "max_tokens": 2048
-        }
+def bedrock_chatbot(text):
+    response = boto3.client('bedrock-runtime').invoke_model(
+        modelId = 'anthropic.claude-3-5-sonnet-20240620-v1:0', 
+        body = json.dumps({
+            'anthropic_version': 'bedrock-2023-05-31',
+            'messages': [ { 'role': 'user', 'content': text } ],
+            'max_tokens': 2048,
+            'temperature': 0.5
+        }),
+        accept='application/json', 
+        contentType='application/json'
     )
-    return bedrock_llm.invoke([("human", input_text)])
+    return json.loads(response.get('body').read())['content'][0]['text']
 
 def lambda_handler(event, context):
     text_input = event['text_input']
     callback_url = event['callback_url']
 
-    result = bedrock_chatbot(text_input).content
+    result = bedrock_chatbot(text_input)
 
     # https://kakaobusiness.gitbook.io/main/tool/chatbot/skill_guide/ai_chatbot_callback_guide#skillresponse
     requests.post(callback_url, json={
--- bedrock-drawing/src/callback.py
+++ bedrock-drawing/src/callback.py
@@ -14,37 +14,31 @@ def lambda_handler(event, context):
     callback_url = event['callback_url']
 
     # https://docs.aws.amazon.com/ko_kr/translate/latest/dg/examples-ct.html
-    english_text = boto3.client(service_name='translate').translate_text(
+    english_text = boto3.client('translate').translate_text(
         Text=text_input, 
         SourceLanguageCode='ko', 
         TargetLanguageCode='en'
     )['TranslatedText']
 
     # https://docs.aws.amazon.com/ko_kr/bedrock/latest/userguide/bedrock-runtime_example_bedrock-runtime_InvokeModel_StableDiffusion_section.html
-    bedrock = boto3.client('bedrock-runtime')
-    seed = random.randint(0, 4294967295)
-    response = bedrock.invoke_model(
+    response = boto3.client('bedrock-runtime').invoke_model(
         modelId='stability.stable-diffusion-xl-v1',
         # https://docs.aws.amazon.com/ko_kr/bedrock/latest/userguide/model-parameters-diffusion-1-0-text-image.html 
         body=json.dumps({
         'text_prompts': [{'text': english_text}],
         'style_preset': 'photographic',
-        'seed': seed,
+        'seed': random.randint(0, 4294967295),
         'cfg_scale': 15,
         'steps': 50,
         })
     )
-    model_response = json.loads(response['body'].read())
-    base64_image_data = model_response['artifacts'][0]['base64']
-    image_data = base64.b64decode(base64_image_data)
+    image_data = base64.b64decode(json.loads(response['body'].read())['artifacts'][0]['base64'])
     image_path = os.path.join('/tmp', 'stability.png')
     with open(image_path, 'wb') as file:
         file.write(image_data)
 
-    s3 = boto3.client('s3')
-    now = datetime.now().strftime('%Y%m%d%H%M%S')
-    image_name = f'{now}_{seed}.png'
-    s3.upload_file(image_path, TEXT_TO_IMAGE_S3_BUCKET_NAME, image_name)
+    image_name = f'{datetime.now().strftime('%Y%m%d%H%M%S')}.png'
+    boto3.client('s3').upload_file(image_path, TEXT_TO_IMAGE_S3_BUCKET_NAME, image_name)
     image_url = f'https://{TEXT_TO_IMAGE_S3_BUCKET_NAME}.s3.{AWS_REGION_NAME}.amazonaws.com/{image_name}'
 
     # https://kakaobusiness.gitbook.io/main/tool/chatbot/skill_guide/ai_chatbot_callback_guide#skillresponse
